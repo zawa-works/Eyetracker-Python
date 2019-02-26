b9549b443c09680488fc6849a10160a886d7f2ad
zawafms <thestoryidontknow@gmail.com>
2019-02-26 17:50:45 +0900

    create fuction: draw_calibration_points

--- calibration.py
+++ calibration.py
@@ -2,10 +2,20 @@ import time
 import tobii_research as tr
 import wx
 import cv2
-# import subprocess
 
 found_eyetrackers = tr.find_all_eyetrackers()
 my_eyetracker = found_eyetrackers[0]
+
+
+def draw_caribration_points(point):
+    img_file_name = 'img/image' + str(point[0]) + '_' + str(point[1]) + '.png'
+    img = cv2.imread(img_file_name)
+    cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(
+        'screen', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.imshow('screen', img)
+    cv2.waitKey(2500)
+
 
 def execute(eyetracker):
     global time, tr
@@ -16,25 +26,15 @@ def execute(eyetracker):
     calibration = tr.ScreenBasedCalibration(eyetracker)
 
     calibration.enter_calibration_mode()
-    print(("Entered calibration mode for eye tracker with serial number {0}.").format(eyetracker.serial_number))
+    print(("Entered calibration mode for eye tracker with serial number {0}.").format(
+        eyetracker.serial_number))
 
-    points_to_calibrate = [(0.5, 0.5), (0.1, 0.1), (0.1, 0.9), (0.9, 0.1), (0.9, 0.9)]
+    points_to_calibrate = [(0.5, 0.5), (0.1, 0.1),
+                           (0.1, 0.9), (0.9, 0.1), (0.9, 0.9)]
 
     for point in points_to_calibrate:
         print(("Show a point on screen at {0}.").format(point))
-
-        img_file_name = 'img/image' + str(point[0]) + '_' + str(point[1]) + '.png'
-        img = cv2.imread(img_file_name)
-        cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
-        cv2.setWindowProperty('screen', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-        height = 1920
-        width = 1080
-
-        circleX = int(width*point[0])
-        circleY = int(height*point[1])
-        cv2.imshow('screen', img)
-        cv2.waitKey(2500)
+        draw_caribration_points(point)
 
         print(("Collecting data at {0}.").format(point))
         if calibration.collect_data(point[0], point[1]) != tr.CALIBRATION_STATUS_SUCCESS:
@@ -42,16 +42,18 @@ def execute(eyetracker):
 
     print("Computing and applying calibration.")
     calibration_result = calibration.compute_and_apply()
-    print(("Compute and apply returned {0} and collected at {1} points.").\
-        format(calibration_result.status, len(calibration_result.calibration_points)))
+    print(("Compute and apply returned {0} and collected at {1} points.").
+          format(calibration_result.status, len(calibration_result.calibration_points)))
 
     print("Computing and applying calibration.")
     calibration_result = calibration.compute_and_apply()
-    print(("Compute and apply returned {0} and collected at {1} points.").\
-        format(calibration_result.status, len(calibration_result.calibration_points)))
+    print(("Compute and apply returned {0} and collected at {1} points.").
+          format(calibration_result.status, len(calibration_result.calibration_points)))
 
     calibration.leave_calibration_mode()
     print("Left calibration mode.")
 
-execute(my_eyetracker)
-exit()
+
+if __name__ == "__main__":
+    execute(my_eyetracker)
+    exit()
