42e86be799fb8dc90e5d46ffea56874c58bc2ea1
zawafms <thestoryidontknow@gmail.com>
2019-03-01 17:38:19 +0900

    add comment

--- calibration.py
+++ calibration.py
@@ -5,10 +5,12 @@ import time
 import tobii_research as tr
 import cv2
 
+# アイトラッカーを見つける
 found_eyetrackers = tr.find_all_eyetrackers()
 my_eyetracker = found_eyetrackers[0]
 
 
+# キャリブレーションに使う点を描画する
 def draw_caribration_points(point):
     img_file_name = 'img/image' + str(point[0]) + '_' + str(point[1]) + '.png'
     img = cv2.imread(img_file_name)
@@ -16,7 +18,7 @@ def draw_caribration_points(point):
     cv2.setWindowProperty(
         'screen', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow('screen', img)
-    cv2.waitKey(2500)
+    cv2.waitKey(2500)  # 2.5秒キャリブレーションする
 
 
 def execute(eyetracker):
@@ -25,12 +27,13 @@ def execute(eyetracker):
     if eyetracker is None:
         return
 
+    # キャリブレーションモードに切り替える
     calibration = tr.ScreenBasedCalibration(eyetracker)
-
     calibration.enter_calibration_mode()
     print(('Entered calibration mode for eye tracker with serial number {0}.').format(
         eyetracker.serial_number))
 
+    # キャリブレーションのための点を表示する座標
     points_to_calibrate = [(0.5, 0.5), (0.1, 0.1),
                            (0.1, 0.9), (0.9, 0.1), (0.9, 0.9)]
 
@@ -39,14 +42,18 @@ def execute(eyetracker):
         draw_caribration_points(point)
 
         print(('Collecting data at {0}.').format(point))
+
+        # 視線をキャリブレートする
         if calibration.collect_data(point[0], point[1]) != tr.CALIBRATION_STATUS_SUCCESS:
             calibration.collect_data(point[0], point[1])
 
+    # キャリブレーションの結果を表示
     print('Computing and applying calibration.')
     calibration_result = calibration.compute_and_apply()
     print(('Compute and apply returned {0} and collected at {1} points.').
           format(calibration_result.status, len(calibration_result.calibration_points)))
 
+    # キャリブレーションモードに終了
     calibration.leave_calibration_mode()
     print('Left calibration mode.')
 
